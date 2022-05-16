82dd23fba7b9d46123843c4e4a89655e4817eb25
MagnusKristian <92784711+MagnusKristian@users.noreply.github.com>
2022-05-16 21:36:23 +0200

    16.mai

---
+++ main-metoden-oppgave1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace main_metoden_oppgave1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] argumenter = args;
+            foreach (var VARIABLE in args)
+            {
+                Console.WriteLine($"Dette logger hvert argument i string array args som man sender inn: {VARIABLE}");
+            }
+            
+        }
+    }
+}
+//HUSK .\ MELLOM NAVNET OG BANEN NÅR DU KJØRER VIA POWERSHELL.
+
+/*
+ Oppgave 1: 
+
+    Send inn 5 argumenter til mainmetoden og print de ut til konsollen
+
+    Hint: Console.WriteLine();
+ */
---
+++ main-metoden-oppgave2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace main_metoden_oppgave2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var VARIABLE in args)
+            {
+                Console.WriteLine($"Dette er alle argumentene: {VARIABLE}");
+            }
+            Console.WriteLine($"Dette er hvor mange argumenter det er: {args.Length}");
+        }
+    }
+}
+
+
+/*
+Oppgave 2: 
+
+Send inn et hvilket som helst antall argumenter til mainmetoden,
+
+og print ut et tall som sier noe om hvor mange argumenter du har sendt inn.
+
+Dersom du sender inn "hei på deg" skal tallet 3 printes til konsollen
+
+Koden skal fungere med et ulikt antall argumenter!
+ */
