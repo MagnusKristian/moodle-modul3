9f5f3d6066f0a2b09e60188dc2b919f631e9a2bd
MagnusKristian <92784711+MagnusKristian@users.noreply.github.com>
2022-05-16 20:38:08 +0200

    16.mai

---
+++ variabeltilordning-oppgave1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace variabeltilordning_oppgave1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer = 123;
+            long longinteger = 1234l;
+            float floatinteger = 12345f;
+            decimal decimalinteger = 123456m;
+            double doubleinteger = 1234567d;
+            string stringword = "hei på deg";
+            char character = 'a';
+            bool boolean = true;    
+
+        }
+    }
+}
+/*
+  Oppgave 1:
+
+    Lag 8 variabler og til-ordne dem en verdi.
+
+    Det skal være en variabel per hver type: int, long, float, decimal, double, string, char og bool
+ */
---
+++ variabeltilordning-oppgave2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace variabeltilordning_oppgave2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Her skal vi plusse sammen to tall. Gi meg det første tallet, det skal være heltall: ");
+            string firstNumber = Console.ReadLine();
+            Console.WriteLine("Og så det andre tallet, det skal være desimaltall: (bruk komma, ikke punktum)");
+            string secondNumber = Console.ReadLine();
+
+            int a = Convert.ToInt32(firstNumber);
+            double b = Convert.ToDouble(secondNumber);
+            var sum = a + b;
+            Console.WriteLine("Tallene dine summert er: "+sum);
+            Console.WriteLine("Svaret på oppgaven er da: 'sum' må være en double eller en mer nøyaktig datatype.");
+        }
+    }
+}
+
+/*
+ Oppgave 2:
+
+    Lag en konsoll-applikasjon som lager 3 variabler. De kan hete a, b og sum. 
+
+    a skal være en int
+    b skal være en decimal. 
+    Verdiene variablene skal ha er:
+
+    a = 5
+    b = 3.0 
+    sum er a + b.
+    Hva slags type variabel må sum være?
+ */
