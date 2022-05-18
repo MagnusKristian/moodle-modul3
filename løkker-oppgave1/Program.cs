3b45028094509ab6d724876b39afbbcdc865516d
MagnusKristian <92784711+MagnusKristian@users.noreply.github.com>
2022-05-18 20:23:11 +0200

    onsdag

---
+++ løkker-oppgave1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace løkker_oppgave1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            Console.WriteLine("Terje er kul!"+(i+1));
+        }
+    }
+}
+
+
+/*
+ Oppgave 1: 
+
+    Lag en for-løkke som printer "Terje er kul" til konsollen 5 ganger
+ */
