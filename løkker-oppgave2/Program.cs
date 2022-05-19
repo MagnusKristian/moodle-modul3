f850d4d8d3cbf43a68a1152467e71db8a1b8cfd2
MagnusKristian <92784711+MagnusKristian@users.noreply.github.com>
2022-05-19 12:30:55 +0200

    torsdag

---
+++ løkker-oppgave2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace løkker_oppgave2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Setning";
+            foreach (char chr in str)
+            {
+                Console.WriteLine(chr);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+/*
+ Oppgave 2:
+
+    Lag en foreach-løkke som går igjennom hver bokstav i en string og printer den til konsollen
+
+    Hint: en string kan brukes på samme måte som et array!
+ */
---
+++ løkker-oppgave3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace løkker_oppgave3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var runWhile = 0;
+            while (runWhile < 10)
+            {
+                runWhile++;
+                Console.WriteLine("Runde nr"+runWhile);
+            }
+            
+        }
+    }
+}
+
+
+/*
+ Oppgave 3:
+
+    Lag en while-løkke som printer ut "Runde nummer:" + et tall som øker med 1 per runde, så lenge rundetelleren er mindre enn 10
+
+    Eks:
+
+    Runde nr 1
+
+    Runde nr 2
+
+    Runde nr 3
+ */
