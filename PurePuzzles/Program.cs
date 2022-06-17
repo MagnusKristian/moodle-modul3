e646abf10a86559fccc4c9744db57b5a7c2af731
MagnusKristian <92784711+MagnusKristian@users.noreply.github.com>
2022-06-17 16:10:18 +0200

    freeedag

---
+++ PurePuzzles/Program.cs
@@ -0,0 +1,275 @@
+﻿namespace PurePuzzles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 2-1 Write a program that produces the following shape:
+                ########
+                 ######
+                  ####
+                   ##
+             */
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.WriteLine();
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.WriteLine();
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.WriteLine();
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+
+            Console.WriteLine();
+            Console.WriteLine("-----------------------");
+            /*
+             * 2-2 Or how about:
+                   ##
+                  ####
+                 ######
+                ########
+                ########
+                 ######
+                  ####
+                   ##   
+             */
+            Console.WriteLine();
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.WriteLine(); 
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.WriteLine();
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.WriteLine();
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.WriteLine();
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.WriteLine();
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.WriteLine(); 
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.WriteLine();
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+
+            Console.WriteLine();
+            Console.WriteLine("-----------------------");
+
+            /*
+             * 2-3 Here’s an especially tricky one:
+                #            #
+                 ##        ##
+                  ###    ###
+                   ########
+                   ########
+                  ###    ###
+                 ##        ##
+                #            # 
+             */
+
+            Console.WriteLine();
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#");
+            Console.WriteLine(); //line1
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.WriteLine(); //line2
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.WriteLine(); //line3
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.WriteLine(); //line4
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.WriteLine(); //line5
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.WriteLine(); //line6
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#");
+            Console.Write("#");
+            Console.Write(" ");
+            Console.WriteLine(); //line7
+            Console.Write("#");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.Write("#"); //line8
+            Console.WriteLine();
+            Console.WriteLine("-----------------------");
+        }
+    }
+}
