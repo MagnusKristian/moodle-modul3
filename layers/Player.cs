51f3b2e3ca0b36f2558ec5d9cdd75d8688762e00
MagnusKristian <92784711+MagnusKristian@users.noreply.github.com>
2022-06-17 15:53:31 +0200

    players

---
+++ layers/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Players
+{
+    internal class Player
+    {
+        public string Name { get; set; }
+        public int Points { get; set; }
+
+        public Player(string name, int points)
+        {
+            Name = name;
+            Points = points;
+        }
+
+        public void Play(Player opponentPlayer)
+        {
+            Points++;
+            opponentPlayer.Points--;
+        }
+
+        public void ShowNameAndPoints()
+        {
+            Console.WriteLine($"Name: {Name}, Points: {Points}.");
+        }
+    }
+}
